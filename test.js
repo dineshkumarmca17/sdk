c3c834a3bb762b2085232195e8fd62641c19bdac
dineshkumarmca17 <dineshkumar.mca17@gmail.com>
2018-02-19 10:58:02 +0530

    also

---
+++ test.js
@@ -0,0 +1,13 @@
+ Volume in drive D is New Volume
+ Volume Serial Number is E041-5573
+
+ Directory of D:\javaproject
+
+02/16/2018  03:43 PM    <DIR>          .
+02/16/2018  03:43 PM    <DIR>          ..
+02/16/2018  02:52 PM               447 index.html
+02/16/2018  03:43 PM                 0 test.js
+02/16/2018  02:38 PM               428 Welcome.class
+02/16/2018  02:52 PM               144 Welcome.java
+               4 File(s)          1,019 bytes
+               2 Dir(s)  313,209,257,984 bytes free
